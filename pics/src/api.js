d24eea8e35816d9ee4d6fe08d60bcb0bfccb4b2c
Sampath kumar <51330761+sampathkumar26@users.noreply.github.com>
2023-01-18 09:35:52 +0530

    Update api.js

--- pics/src/api.js
+++ pics/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchImages = async (term) => {
    const response = await axios.get( 'https://api.unsplash.com/search/photos', {
         headers: {
-            Authorization: 'Client-ID vO4MobYiMWMJwX4KcLiH7mSg4Jcf1087E67Ro3_dFkI',
+            Authorization: 'API KEY',
         },
         params: {
             query: term,
